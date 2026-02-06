a4530ff82996aea2af4fc9c963972b574b7ce782
unknown <prizekeyz@gmail.com>
2026-02-06 14:42:12 +0100

    Fix

--- constants.ts
+++ constants.ts
@@ -28,7 +28,8 @@ export const CONFIG: SiteConfig = {
     },
     {
       id: 2,
-      url: "https://i.postimg.cc/MHBThJTD/Whats_App_Image_2026_02_06_at_12_09_52_(2).jpg",
+      url: "https://i.postimg.cc/C581Wp1C/Whats_App_Image_2026_02_06_at_12_09_52_(5).jpg",
+      title: "Chi and Bee"
       caption: "I can't remember what the moment was but I was happy to be with you."
     },
     {
@@ -39,7 +40,7 @@ export const CONFIG: SiteConfig = {
     },
     {
       id: 4,
-      url: "https://i.postimg.cc/C581Wp1C/Whats_App_Image_2026_02_06_at_12_09_52_(5).jpg",
+      url: "https://i.postimg.cc/xdNWGf4S/Whats-App-Image-2026-02-06-at-12-09-52.jpg",
       title: "After Church Shenanigans",
       caption: "One of the things I miss about us when we are apart is our after church shenanigans. I hope we can have more of those moments together."
     }
@@ -50,5 +51,5 @@ export const CONFIG: SiteConfig = {
   
   // 6. Background music URL (MP3/OGG)
   // Note: Ensure the link is direct to an audio file
-  musicUrl: "public/music/videoplayback.mp3"
+  musicUrl: "<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2261346776%3Fsecret_token%3Ds-TPdy19s2Rzj&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/ojiaku-chidera-francis" title="Ojiaku Chidera Francis" target="_blank" style="color: #cccccc; text-decoration: none;">Ojiaku Chidera Francis</a> · <a href="https://soundcloud.com/ojiaku-chidera-francis/valentine-song/s-TPdy19s2Rzj" title="Valentine Song" target="_blank" style="color: #cccccc; text-decoration: none;">Valentine Song</a></div>"
 };
